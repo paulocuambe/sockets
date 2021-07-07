65d2c3e9855a8617e75a41f68b55a6499d4cc624
paulocuambe <paulo.cuambe@sciafrica.net>
2021-07-07 18:11:52 +0200

    reproduce socket read timeout error and the fix

--- src/main/java/threaded/multi/Client.java
+++ src/main/java/threaded/multi/Client.java
@@ -6,11 +6,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Socket socket = new Socket(Constant.IP, Constant.PORT);
-
+        socket.setSoTimeout(2000);
         String product = "a";
 
         InputStream inputStream = socket.getInputStream();
@@ -19,10 +20,21 @@ public class Client {
         outputStream.write(product.getBytes());
 
         byte[] response = new byte[100];
-        inputStream.read(response);
+
+        int i = 1;
+        do {
+            try {
+                inputStream.read(response);
+                break;
+            } catch (SocketTimeoutException exception) {
+                System.out.println(String.format("Erro - %d", i));
+                i++;
+            }
+        } while (i <= 2);
+
 
         String strResponse = new String(response).trim();
-        System.out.printf("Returned response is - %s", strResponse);
+        System.out.printf("\nReturned response is - %s", strResponse);
         socket.close();
     }
 }
--- src/main/java/threaded/multi/Server.java
+++ src/main/java/threaded/multi/Server.java
@@ -7,13 +7,14 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         ServerSocket serverSocket = new ServerSocket(Constant.PORT);
         System.out.printf("Listening on port %d%n", Constant.PORT);
 
         while (true) {
             System.out.println("\nWaiting for the client...");
             Socket socket = serverSocket.accept();
+            Thread.sleep(2001);
 
             System.out.println("Starting a thread wich will service the client");
             new ServiceThread(socket).start();
